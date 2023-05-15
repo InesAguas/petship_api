264cf08d166657f51223fc62ea5041c7b68547db
Steph <stephanesq1@gmail.com>
2023-05-15 16:35:45 +0100

    horario

--- database/migrations/2023_03_28_170544_create_utilizadores_table.php
+++ database/migrations/2023_03_28_170544_create_utilizadores_table.php
@@ -25,6 +25,7 @@ return new class extends Migration
             $table->string('website')->nullable();
             $table->string('facebook')->nullable();
             $table->string('instagram')->nullable();
+            $table->string('horario')->nullable();
             $table->timestamps();
         });
     }
